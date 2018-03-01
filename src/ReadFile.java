0e8d8a195618a18c77f732bed846941870db2007
Swapnil Raj <raj.swapnil99@gmail.com>
2018-03-01 18:33:00 +0000

    Add readfile Assuming Car and Ride exist

---
+++ src/ReadFile.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class ReadFile {
+  public void readFile(String fileName) throws IOException{
+    Scanner input = new Scanner(new File(fileName));
+    int R, C, F, N, B, T;
+
+    String[] temp = input.nextLine().split(" ");
+    R = tI(temp[0]);
+    C = tI(temp[1]);
+    F = tI(temp[2]);
+    N = tI(temp[3]);
+    B = tI(temp[4]);
+    T = tI(temp[5]);
+
+    ArrayList<Ride> rides = new ArrayList<Ride>(N);
+
+    ArrayList<Car> cars = new ArrayList<Car>(F);
+    for(int i = 0; i < F; i++) {
+      cars.add(new Car(i));
+    }
+
+    for(int i = 0; i < N; ++i) {
+      temp = input.nextLine().split(" ");
+      rides.add(new Ride(tI(temp[0]), tI(temp[1]), tI(temp[2]), tI(temp[3]), tI(temp[4]), tI(temp[5])));
+    }
+
+  }
+
+  public int tI(String n) {
+    return Integer.parseInt(n);
+  }
+}
