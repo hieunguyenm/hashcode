ddc8c4e29b1f96d43f532df4f67d4820bb376ea0
Hieu Nguyen <hieunguyenvn1@gmail.com>
2018-03-01 18:29:14 +0000

    Add car jobs tracker

---
+++ src/Jobs.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Jobs {
+    private HashMap<Integer, ArrayList<Integer>> jobs;
+
+    public Jobs(int cars) {
+        jobs = new HashMap<>();
+
+        for (int i = 0; i < cars; i++) {
+            jobs.put(i, new ArrayList<>());
+        }
+    }
+
+    public ArrayList<Integer> getRides(int car) {
+        return jobs.get(car);
+    }
+
+    public void insertRide(int car, int ride) {
+        ArrayList<Integer> carJobs = jobs.get(car);
+        carJobs.add(ride);
+        jobs.put(car, carJobs);
+    }
+}
