2f98eae3ef8782f32001f3e29ea1a6324b5a21dc
Hieu Nguyen <hieunguyenvn1@gmail.com>
2018-03-01 18:51:48 +0000

    Add Main and Ride classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class Main {
+    public static final String FILE = "a_example.in";
+
+    public static void main(String[] args) {
+        ArrayList<Ride> rides = null;
+        ArrayList<Car> cars = null;
+        ReadFile reader = new ReadFile();
+
+        try {
+            reader.readFile(FILE, rides, cars);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
