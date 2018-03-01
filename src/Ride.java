49328463a49ff0e6649ffa8976517291901b812b
Hieu Nguyen <hieunguyenvn1@gmail.com>
2018-03-01 18:46:12 +0000

    Add Main and Ride classes

---
+++ src/Ride.java
@@ -0,0 +1,12 @@
+public class Ride {
+    public int a, b, x, y, start, finish;
+
+    public Ride(int a, int b, int x, int y, int start, int finish) {
+        this.a = a;
+        this.b = b;
+        this.x = x;
+        this.y = y;
+        this.start = start;
+        this.finish = finish;
+    }
+}
