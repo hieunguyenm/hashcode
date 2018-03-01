20ac8a542078b1a86986ec9e1886d74f3bb2ce29
Swapnil Raj <raj.swapnil99@gmail.com>
2018-03-01 21:18:47 +0000

    Add condition

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.PriorityQueue;
 
 public class Main {
-    private static final String FILE = "a_example.in";
+    private static final String FILE = "c_no_hurry.in";
 
     private static ArrayList<Ride> rides = new ArrayList<>();
     private static ArrayList<Car> cars = new ArrayList<>();
@@ -58,7 +58,21 @@ public class Main {
         int currentDist = Integer.MAX_VALUE;
 
         for (Ride r : ridesCopy) {
-            if (Math.abs(r.a - c.currentRide.x) + Math.abs(r.b - c.currentRide.y) < currentDist) {
+            if (Math.abs(r.a - c.currentRide.x) + Math.abs(r.b - c.currentRide.y) < currentDist
+                && c.currentTick +  Math.abs(c.currentRide.x - r.a) + Math.abs(c.currentRide.y - r.b) + r.distance < r.finish) {
+                closest = r.id;
+            }
+        }
+
+        return closest;
+    }
+
+    private static int getNext1(Car c) {
+        int closest = 0;
+        int currentDist = Integer.MAX_VALUE;
+
+        for (Ride r : ridesCopy) {
+            if (r.distance < currentDist && c.currentTick +  Math.abs(c.currentRide.x - r.a) + Math.abs(c.currentRide.y - r.b) + r.distance < r.finish) {
                 closest = r.id;
             }
         }
