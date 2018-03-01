430f664e3ad970d12d1a49002445c899987c94cb
Swapnil Raj <raj.swapnil99@gmail.com>
2018-03-01 18:48:45 +0000

    Add distance to ride

--- src/Ride.java
+++ src/Ride.java
@@ -1,5 +1,6 @@
 public class Ride {
     public int a, b, x, y, start, finish;
+    int distance;
 
     public Ride(int a, int b, int x, int y, int start, int finish) {
         this.a = a;
@@ -8,5 +9,7 @@ public class Ride {
         this.y = y;
         this.start = start;
         this.finish = finish;
+        this.distance = Math.abs(a - x) + Math.abs(b - y);
     }
+
 }
