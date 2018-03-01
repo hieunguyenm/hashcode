896ebb77201375b0f5cc50d072aaf500218c9726
Swapnil Raj <raj.swapnil99@gmail.com>
2018-03-01 19:32:32 +0000

    Add comparable to Car

--- src/Car.java
+++ src/Car.java
@@ -1,7 +1,8 @@
-public class Car {
+public class Car implements Comparable<Car> {
   public int id;
   public Ride currentRide;
   public boolean taken;
+  public int currentTick;
 
   public Car(int id) {
     this.id = id;
@@ -10,7 +11,11 @@ public class Car {
 
   public void takeRide(Ride r) {
     this.currentRide = r;
+    this.currentTick += r.distance;
     taken = true;
   }
 
+  @Override public int compareTo(Car o) {
+    return this.currentTick - o.currentTick;
+  }
 }
